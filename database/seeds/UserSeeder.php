6eb72114cc0468a965362074aca9962871abc5a7
João Vitor Barbosa Souza <jvbs98@hotmail.com>
2020-05-01 01:44:41 -0300

    criada seed de usuario e rota de autenticacao tratada

---
+++ database/seeds/UserSeeder.php
@@ -0,0 +1,21 @@
+<?php
+
+use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\DB;
+
+class UserSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        DB::table('users')->insert([
+            'name' => 'Administrador Surferzone',
+            'email' => 'admin@surferzone.com.br',
+            'password' => Hash::make('surferzone'),
+        ]);
+    }
+}
