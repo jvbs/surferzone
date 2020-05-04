22573184d81424086a789ecac632fc93ccb8afb7
João Vitor Barbosa Souza <jvbs98@hotmail.com>
2020-05-03 21:30:53 -0300

    bootstrap in library

--- resources/views/layouts/template.blade.php
+++ resources/views/layouts/template.blade.php
@@ -7,7 +7,7 @@
     <title>Surferzone - @yield('title')</title>
     <link rel="icon" href="assets/img/favicon.ico" />
     <!-- Bibliotecas CSS -->
-    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css"/>
+    <link href="{{ asset('css/app.css') }}" rel="stylesheet">
     <link href="{{ asset('css/style.css') }}" rel="stylesheet">
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css" />
     <!--[if lt IE 9]>
