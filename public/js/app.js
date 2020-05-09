dc8b69b905be7cf4809192de4cec4db27a2e6cec
João Vitor Barbosa Souza <jvbs98@hotmail.com>
2020-05-09 02:10:36 -0300

    arquivos css do admin

--- public/js/app.js
+++ public/js/app.js
@@ -37393,8 +37393,8 @@ window.axios.defaults.headers.common['X-Requested-With'] = 'XMLHttpRequest';
 /*! no static exports found */
 /***/ (function(module, exports, __webpack_require__) {
 
-__webpack_require__(/*! /home/jvbs/Desktop/College 🎓/PI/surferzone/resources/js/app.js */"./resources/js/app.js");
-module.exports = __webpack_require__(/*! /home/jvbs/Desktop/College 🎓/PI/surferzone/resources/sass/app.scss */"./resources/sass/app.scss");
+__webpack_require__(/*! C:\Users\João Vitor\Desktop\Faculdade\Pi\surferzone\resources\js\app.js */"./resources/js/app.js");
+module.exports = __webpack_require__(/*! C:\Users\João Vitor\Desktop\Faculdade\Pi\surferzone\resources\sass\app.scss */"./resources/sass/app.scss");
 
 
 /***/ })
